db97e242bc77149d03d2c620da65f41e8740e1e2
lavanyaSarang <lavanyakarunamoorthy87@gmail.com>
2022-03-11 14:25:38 +0530

    Commit1

---
+++ src/test/java/Tests_POST.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+import org.testng.annotations.Test;
+
+import com.google.gson.JsonObject;
+
+import io.restassured.internal.path.json.JSONAssertion;
+
+public class Tests_POST {
+	
+	@Test
+	public void test1_post() {
+	
+		Map <String , Object> map=new HashMap <String , Object>();
+		map.put("name", "Lavanya");
+		map.put("job", "QA");
+		System.out.println(map);
+		JSON request=new JsonObject(map);
+		//request.put()
+		
+	}
+
+}
